70e39c80cb0a37f117fad0645bdad275ce78ba15
tauantcamargo <lukyhenson@gmail.com>
2023-06-17 23:38:51 -0300

    test: adding more tests to category unit test

--- src/category/domain/entities/category.spec.ts
+++ src/category/domain/entities/category.spec.ts
@@ -19,4 +19,103 @@ describe("Category Unit Tests", () => {
     expect(category.props).toStrictEqual(props);
     expect(category.props.created_at).toBe(category.created_at);
   });
+
+  test("getter of name field", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+      is_active: true,
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.name).toBe("Movie");
+  });
+
+  test("getter and setter of descripition field", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+      description: "Movies category",
+      is_active: true,
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.description).toBe("Movies category");
+  });
+
+  test("getter and setter of descripition field should be null", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+      is_active: true,
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.description).toBeNull();
+  });
+
+  test("getter and setter of is_active field", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+      is_active: true,
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.is_active).toBeTruthy();
+  });
+
+  test("getter and setter of is_active field should be true", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.is_active).toBeTruthy();
+  });
+
+  test("getter and setter of created_at field", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+      is_active: true,
+      created_at: new Date(),
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+
+  test("getter and setter of created_at field should be instance of Date", () => {
+    // Arrange
+    const props = {
+      name: "Movie",
+      is_active: true,
+    }; 
+
+    // Act
+    const category: Category = new Category(props);
+
+    // Assert
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
 });
